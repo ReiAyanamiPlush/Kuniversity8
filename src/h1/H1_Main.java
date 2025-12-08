4973741a0a949ec2d94b0ba9a019b74a01a11af3
ReiAyanamiPlush <bozorgmehrkazemieh@gmail.com>
2025-12-08 15:23:05 +0100

    h1

--- src/h1/H1_Main.java
+++ src/h1/H1_Main.java
@@ -4,10 +4,10 @@ public class H1_Main {
 
     public static void main(String[] args) {
 
-        Bruch h = new Bruch(4,3);
+        Bruch h = new Bruch(25,5);
+        Bruch b = new Bruch(5,1);
+        System.out.println(h.hasSameValueAs(b));
 
-        h.shorten();
-        h.print();
 
 
     }
